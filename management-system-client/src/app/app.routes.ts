0bc5e3bfec5fa602d72ae4f93ae4681ca269eb2c
Frederico <frederico.enzo@hotmail.com>
2024-11-04 23:22:00 -0300

    error: nao esta mudando de rota

--- management-system-client/src/app/app.routes.ts
+++ management-system-client/src/app/app.routes.ts
@@ -1,12 +1,18 @@
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 import { IndexComponent } from './view/lyate/index/index.component';
 import { ManegerMedicoComponent } from './view/componentes/maneger-medico/maneger-medico.component';
 import { ManegerPacienteComponent } from './view/componentes/maneger-paciente/maneger-paciente.component';
+import { NgModule } from '@angular/core';
 
 export const routes: Routes = [
     { path: "", component: IndexComponent, children: [
         {path:"manegar-medico",component: ManegerMedicoComponent, },
         {path:"manegar-paciente",component: ManegerPacienteComponent, }
      ] }
-
 ];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+  })
+  export class AppRoutingModule { }
