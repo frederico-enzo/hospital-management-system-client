14ec4348090a2c063a3860b9c5b3f90619b76619
Frederico <frederico.enzo@hotmail.com>
2024-11-07 11:29:33 -0300

    feat: mudança na estilizadaçao do header e mudanda e adiçao de rotas

--- management-system-client/src/app/app.routes.ts
+++ management-system-client/src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { IndexComponent } from './view/lyate/index/index.component';
 import { ManegerMedicoComponent } from './view/componentes/maneger-medico/maneger-medico.component';
 import { ManegerPacienteComponent } from './view/componentes/maneger-paciente/maneger-paciente.component';
 import { NgModule } from '@angular/core';
+import { ManegerHomeComponent } from './view/componentes/maneger-home/maneger-home.component';
 
 export const routes: Routes = [
     { path: "", component: IndexComponent, children: [
-        {path:"manegar-medico",component: ManegerMedicoComponent, },
-        {path:"manegar-paciente",component: ManegerPacienteComponent, }
+        {path:"maneger-home",component: ManegerHomeComponent, },
+        {path:"maneger-medico",component: ManegerMedicoComponent, },
+        {path:"maneger-paciente",component: ManegerPacienteComponent, }
      ] }
 ];
 
