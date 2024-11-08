5d3ef315191fd3e432e8f7753566816af97d6516
Frederico <frederico.enzo@hotmail.com>
2024-11-08 19:26:26 -0300

    feat: inicio do layate do login

--- management-system-client/src/app/app.routes.ts
+++ management-system-client/src/app/app.routes.ts
@@ -4,8 +4,11 @@ import { ManegerMedicoComponent } from './view/componentes/maneger-medico/manege
 import { ManegerPacienteComponent } from './view/componentes/maneger-paciente/maneger-paciente.component';
 import { NgModule } from '@angular/core';
 import { ManegerHomeComponent } from './view/componentes/maneger-home/maneger-home.component';
+import { LoginComponent } from './view/system/login/login.component';
 
 export const routes: Routes = [
+
+    { path: "login", component: LoginComponent},
     { path: "", component: IndexComponent, children: [
         {path:"",component: ManegerHomeComponent, },
         {path:"maneger-medico",component: ManegerMedicoComponent, },
